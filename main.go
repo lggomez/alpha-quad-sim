908e95f3ca8fcc2fceeb1eff8887d8723d1c99b3
Luis G <luis.gg1990@gmail.com>
2017-04-08 17:54:26 -0300

    Remove dead code and unify routing

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-	"github.com/gorilla/mux"
 	"strconv"
 	"os"
 	"google.golang.org/appengine"
@@ -14,35 +13,33 @@ import (
 const WelcomeMessage = "Welcome to the climate registry. Please access the clima API via 'URL/clima/day' (day is an int)"
 
 type Response struct {
+	Path string
 	Climate string `json:"clima"`
 	Day     int    `json:"dia"`
 }
 
 func main() {
-	http.HandleFunc("/", indexHandle)
-	http.HandleFunc("/clima/", climaHandle)
+	mux := http.NewServeMux()
+
+	// Expose REST api per bonus requirement
+	mux.HandleFunc("/", indexHandle)
+	mux.HandleFunc("/clima/", climaHandle)
+	mux.HandleFunc("/clima", climaHandle)
+	http.Handle("/", mux)
 	appengine.Main()
-
-	if verifyOfflineMode() {
-		// Print simulation status per requirement
-		days := 3650
-		sim := NewSimulation()
-		sim.Simulate(days, NewSimluatorConfig(true, false))
-
-		// Expose REST api
-		router := mux.NewRouter()
-		router.HandleFunc("/", IndexEndpoint).Methods("GET")
-		router.HandleFunc("/clima/{dia:[0-9]+}", GetClimateEndpoint).Methods("GET")
-		router.Queries("{dia:[0-9]+}")
-
-		return
-	}
 
 	initDb, days := verifyInitializeDbMode()
 	if initDb {
 		// Persist simulation status per bonus requirement
 		sim := NewSimulation()
 		sim.Simulate(days, NewSimluatorConfig(false, true))
+	}
+
+	if verifyOfflineMode() {
+		// Print simulation status per requirement
+		days := 3650
+		sim := NewSimulation()
+		sim.Simulate(days, NewSimluatorConfig(true, false))
 	}
 }
 
@@ -58,62 +55,39 @@ func indexHandle(w http.ResponseWriter, r *http.Request) {
 }
 
 func climaHandle(w http.ResponseWriter, r *http.Request) {
-	dayParam := strings.TrimPrefix(r.URL.Path, "/clima/")
+	dayParam := r.URL.Query().Get("dia")
+
+	if dayParam == ""  {
+		dayParam = strings.TrimPrefix(r.URL.Path, "/clima/")
+	}
+
+	response := &Response{}
 	dayIntParam := int64(0)
 	dayIntParam, err := strconv.ParseInt(dayParam, 10, 64)
 
-	response := getClimateResponse(err, dayIntParam)
-	json.NewEncoder(w).Encode(response)
-}
-
-func IndexEndpoint(w http.ResponseWriter, req *http.Request) {
-	json.NewEncoder(w).Encode(WelcomeMessage)
-	return
-}
-
-func GetClimateEndpoint(w http.ResponseWriter, req *http.Request) {
-	params := mux.Vars(req)
-	days, err := strconv.ParseInt(params["dia"], 10, 64)
-	response := &Response{}
-
-	if err == nil {
-		sim := NewSimulation()
-		climate := sim.Simulate(int(days), NewSimluatorConfig(false, false))
-
+	if err != nil {
 		response = &Response{
-			Climate: climate,
-			Day:     int(days),
-		}
-
-	} else {
-		response = &Response{
-			Climate: "invalid value",
+			Climate: "invalid value in path " + r.URL.Path,
 			Day:     -1,
 		}
+	} else {
+		response = getClimateResponse(dayIntParam)
 	}
 
-
 	json.NewEncoder(w).Encode(response)
 }
 
-func getClimateResponse(err error, dayIntParam int64) *Response {
+func getClimateResponse(dayIntParam int64) *Response {
 	response := &Response{}
 
-	if err == nil {
-		days := int(dayIntParam)
+	days := int(dayIntParam)
 
-		sim := NewSimulation()
-		climate := sim.Simulate(int(days), NewSimluatorConfig(false, false))
+	sim := NewSimulation()
+	climate := sim.Simulate(int(days), NewSimluatorConfig(false, false))
 
-		response = &Response{
-			Climate: climate,
-			Day:     int(days),
-		}
-	} else {
-		response = &Response{
-			Climate: "invalid value",
-			Day:     -1,
-		}
+	response = &Response{
+		Climate: climate,
+		Day:     int(days),
 	}
 
 	return response
