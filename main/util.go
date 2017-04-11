39c39d20f65c9828fe75afabe9142d37210bb199
lgomez <lgomez@hexacta.com>
2017-04-11 11:56:09 -0300

    no message

---
+++ main/util.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"errors"
+	"math"
+)
+
+// GetEnvironmentVariable - Validate and get and environment variable value
+func GetEnvironmentVariable(k string) (string, error) {
+	v := os.Getenv(k)
+	err := error(nil)
+
+	if v == "" {
+		message := fmt.Sprintf("%s environment variable not set.", k)
+		err = errors.New(message)
+	}
+
+	return v, err
+}
+
+// AreApproximatelyEqual - Returns float equalty within a threshold of 1e-3
+func AreApproximatelyEqual(a float32, b float32) bool {
+	return math.Abs(float64(a-b)) < 0.001
+}
